3025812b6296dccdd88805def009e1d5fbf0fe9b
benjaguilar25 <benjaguilar25@gmail.com>
2022-06-12 22:09:18 -0400

    lero lero

--- hadoop/src/org/mdp/hadoop/cli/CountReviews.java
+++ hadoop/src/org/mdp/hadoop/cli/CountReviews.java
@@ -13,15 +13,12 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.mdp.hadoop.cli.CountWords.CountWordsMapper;
-import org.mdp.hadoop.cli.CountWords.CountWordsReducer;
 
-public class CoStars {
-	public static class CoStarsMapper extends Mapper<Object, Text, Text, IntWritable>{
+public class CountReviews {
+	public static class AnimeReviewMapper extends Mapper<Object, Text, Text, IntWritable>{
 		private final IntWritable one = new IntWritable(1);
 
 		
@@ -39,39 +36,36 @@ public class CoStars {
 			String content = value.toString();
 			String[] lines = content.split("\n");
 			String[] values;
-			Hashtable<String, List<String>> movieDict = new Hashtable<String, List<String>>();
-			String actorName, movieKey;
-			int nMovies = 0;
+			Hashtable<String, List<Double>> animeDict = new Hashtable<String, List<Double>>();
+			String animeKey;
+			Double animeScore;
+			int nAnime = 0;
 			for(String line:lines) {
-				values = line.split("\t");
-				if(values[4]=="THEATRICAL_MOVIE") {
-					actorName = values[0];
-					nMovies += 1;
-					movieKey = values[1] + "#" + values[2] + "#" + values[3];
-					if(movieDict.containsKey(movieKey)) {
-						movieDict.get(movieKey).add(actorName);
-					}else {
-						List<String> actors = new ArrayList<String>();
-						actors.add(actorName);
-						movieDict.put(movieKey, actors);
-					}
+				values = line.split(",");
+				animeKey = values[3];
+				animeScore = Double.parseDouble(values[8]);
+				nAnime += 1;
+				if(animeDict.containsKey(animeKey)) {
+					animeDict.get(animeKey).add(animeScore);
+				}
+				else {
+					List<Double> scores = new ArrayList<Double>();
+					scores.add(animeScore);
+					animeDict.put(animeKey, scores);
 				}
 			}
-			Set<String> setOfMovies = movieDict.keySet();
-			for(String movie:setOfMovies) {
-				List<String> actors = movieDict.get(movie);
-				int l = actors.size();
+			Set<String> setOfAnimes = animeDict.keySet();
+			for(String anime:setOfAnimes) {
+				List<Double> scores = animeDict.get(anime);
+				int l = scores.size();
 				for(int i = 0; i < l; i++) {
-					String actor = actors.get(i);
-					for(int j = 1; j < l-i; j++) {
-						output.write(new Text(actor + "#" + actors.get(i+j)), one);
-					}
+					output.write(new Text(anime), one);
 				}
 			}
 		}
 	}
 
-	public static class CoStarsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class AnimeReviewReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 			/**
 			 * Given a key (actor1#actor2) and all values (partial counts) for 
@@ -99,15 +93,13 @@ public class CoStars {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2) {
-			System.err.println("Usage: "+CountWords.class.getName()+" <in> <out>");
-			System.exit(2);
+		String inputLocation = args[0];
+		String outputLocation = args[1];
+
+		for (String s: args) {
+			System.out.println(s);
 		}
-		String inputLocation = otherArgs[0];
-		String outputLocation = otherArgs[1];
-		
+
 		Job job = Job.getInstance(new Configuration());
 	     
 	    FileInputFormat.setInputPaths(job, new Path(inputLocation));
@@ -118,11 +110,11 @@ public class CoStars {
 	    job.setMapOutputKeyClass(Text.class);
 	    job.setMapOutputValueClass(IntWritable.class);
 	    
-	    job.setMapperClass(CoStarsMapper.class);
-	    job.setCombinerClass(CoStarsReducer.class); // in this case a combiner is possible!
-	    job.setReducerClass(CoStarsReducer.class);
+	    job.setMapperClass(AnimeReviewMapper.class);
+	    job.setCombinerClass(AnimeReviewReducer.class); // in this case a combiner is possible!
+	    job.setReducerClass(AnimeReviewReducer.class);
 	     
-	    job.setJarByClass(CountWords.class);
-		job.waitForCompletion(true);
+	    //job.setJarByClass(CountWords.class);
+		//job.waitForCompletion(true);
 	}
 }
